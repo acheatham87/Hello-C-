e47dd4bd3b41122db5cb5f4af09c6be372462633
acheatham87 <acheatham87@gmail.com>
2022-05-27 13:12:35 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if(string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else 
+{
+    Console.WriteLine($"Hello {name}!");
+}
